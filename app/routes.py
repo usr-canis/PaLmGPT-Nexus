17788d039b3f5f6e15b2558189d8633755647951
Niranjan <75774547+usr-canis@users.noreply.github.com>
2023-05-22 21:50:35 +0530

    regular push

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 from bardapi import Bard
-import os
 
 token="None"
 
@@ -10,10 +9,7 @@ def index():
     if request.method == 'POST':
         global token 
         token = request.form.get('psid')
-        # Process the key and retrieve data based on it
-
-
-        
+        # Process the key and retrieve data based on it     
         return redirect(url_for('chat'))
 
     return render_template('index.html')
@@ -23,8 +19,10 @@ def chat():
     if request.method == 'POST':
         prompt = request.form.get('prompt')
         #call
-
-
+        global token
+        print(token)
+        bard=Bard(token=token)
+        bard.get_answer(prompt)['content']
         
 
     return render_template('chat.html')
